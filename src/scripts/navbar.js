88cc1fc9336663c2f1cfcbc89885e3dd15d2200d
Kacper Śleziak <kacper-sleziak@wp.pl>
2021-08-25 18:05:15 +0200

    Nav small fixes

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -3,12 +3,8 @@ var close_buton = document.getElementById("close_button");
 var links_container = document.getElementById("links_container");
 var side_nav = document.getElementById("side_navbar");
 
-function menu_button() {
-    open_button.classList.toggle("change");
-    open_side_nav();
-}
-
 function open_side_nav() {
+    open_button.classList.toggle("change");
     links_container.style.opacity = "1";
     side_nav.style.width = "150px";
 }
@@ -19,5 +15,5 @@ function close_side_nav() {
     side_nav.style.width = "0";
 }
 
-open_button.addEventListener("click", menu_button);
+open_button.addEventListener("click", open_side_nav);
 close_buton .addEventListener("click", close_side_nav);
